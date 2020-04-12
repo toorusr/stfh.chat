6e436125a124851c524604965443827ad3e34671
Max <hello@max.berlin>
2020-04-12 10:51:40 +0200

    Add: navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+<nav class="px-10 mb-16 mt-10 flex justify-between flex-row">
+  <div class="flex flex-row">
+    <img class="h-10 mr-4" src="@/assets/img/logo.svg" />
+    <p class="bree text-green-400 my-auto text-xs">Stay the fuck home</p>
+  </div>
+  <div class="flex flex-row">
+    <img class="mx-2 h-10" src="@/assets/img/app-store.svg" />
+    <img class="h-10" src="@/assets/img/play-store.svg" />
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'PersonCard',
+  props: {
+    name: String,
+    title: String,
+    website: String,
+    handle: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
