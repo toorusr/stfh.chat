2bc28d260758650f01b2c11fd9e8cda1115ed490
Max <hello@max.berlin>
2020-04-12 10:50:50 +0200

    Add: open sans font

--- src/App.vue
+++ src/App.vue
@@ -14,23 +14,10 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 15px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
