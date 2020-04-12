1a04ff1b6c7bb829dc79e7b717f45bcb51080b3e
Max <hello@max.berlin>
2020-04-12 15:44:33 +0200

    Add: navbar router-link to home

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
 <nav class="px-10 mb-16 mt-10 flex justify-between flex-row">
-  <div class="flex flex-row">
+  <router-link to="/"><div class="flex flex-row">
     <img class="h-10 mr-4" src="@/assets/img/logo.svg" />
     <p class="bree text-green-400 my-auto text-xs">Stay the fuck home</p>
-  </div>
+  </div></router-link>
   <div class="flex flex-row">
     <img class="mx-2 h-10" src="@/assets/img/app-store.svg" />
     <img class="h-10" src="@/assets/img/play-store.svg" />
@@ -16,7 +16,3 @@ export default {
   name: 'NavBar',
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
